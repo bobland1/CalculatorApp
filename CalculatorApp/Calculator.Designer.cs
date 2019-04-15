60b189ead3ae010e2ed4f1bf1e5fdd367e9268ab
Daniel Riddell <daniel.riddell@simplyhealth.co.uk>
2019-04-15 15:58:59 +0100

    Sucessfully converted into a windows application

---
+++ CalculatorApp/Calculator.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CalculatorApp
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEquation = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEquation
+            // 
+            this.txtEquation.Location = new System.Drawing.Point(84, 79);
+            this.txtEquation.Name = "txtEquation";
+            this.txtEquation.Size = new System.Drawing.Size(100, 20);
+            this.txtEquation.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(85, 218);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(94, 134);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(77, 48);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(86, 49);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(5);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(99, 13);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "Enter your equation";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 456);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtEquation);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEquation;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process();
+        }
+
+        private void Process()
+        {
+            try
+            {
+                var input = txtEquation.Text;
+
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    lblMessage.Text = "Exiting..";
+                    Environment.Exit(-1);
+                }
+
+                var equation = EquationHelper.RemoveWhitespace(input);
+
+                if (EquationValidator.IsEquationValid(equation))
+                {
+                    var result = EquationTreeGenerator.Calculation(equation).getValue();
+                    txtResult.Text = result.ToString();
+                    txtResult.Refresh();
+
+
+                    lblMessage.Text = "Enter another equation \n or Type 'exit' to exit";
+                    lblMessage.Refresh();
+                }
+                else
+                {
+                    lblMessage.Text = "Invalid Equation! Please try again";
+                    lblMessage.Refresh();
+                }
+            }
+            catch (InvalidEquationException e)
+            {
+                lblMessage.Text = e.Message;
+            }
+        }
+    }
+}
